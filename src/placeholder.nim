b69384eba3709b8db9f590c89d427d40fd15f10b
András Németh <sunarch@protonmail.com>
2024-02-29 12:07:40 +0100

    Add command line options with help

--- src/placeholder.nim
+++ src/placeholder.nim
@@ -1,25 +1,119 @@
+import std/parseopt as po
+import std/times as times
 from std/strformat import fmt
-
-# prototype:
-# (this is a placeholder to keep the timeline from clearing)
+from system import quit
 
 const
-    use_this = true
-    this = "this is a"
-    use_parens = true
-    verb = "keep"
-    particle = "from"
-    article = "the"
-    noun = "timeline"
-    last_part = "clearing"
+  DEBUG = false
+
+const
+  PROGRAM_NAME = "placeholder"
+  VERSION_MAJOR = 0
+  VERSION_MINOR = 2
+  VERSION_PATCH = 0
+  COPYRIGHT_YEARS = "2024"
+  COPYRIGHT_NAME = "András Németh"
+
+proc version(full = false): string =
+  result = fmt"{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}"
+  if full:
+    result = fmt"{PROGRAM_NAME} v{result}"
+
+const
+  PROTOTYPE = "(this is a placeholder to keep the timeline from clearing)"
 
 var
-    output = fmt"placeholder to {verb} {article} {noun} {particle} {last_part}"
+  show_help_only = false
+  show_version_only = false
+  show_prototype = false
+  use_parens = true
+  use_this = true
+
+proc current_date(): string =
+  result = times.now().format("yyyy-MM-dd")
+
+proc show_help() =
+  echo(version(full=true))
+  echo(fmt"Compiled on {current_date()}")
+  echo(fmt"Copyright (c) {COPYRIGHT_YEARS} by {COPYRIGHT_NAME}")
+  echo()
+  echo("    {PROGRAM_NAME} [options]")
+  echo()
+  echo("Options:")
+  echo("  --help       Show this help and exit")
+  echo("  --version    Show version information and exit")
+  echo("  --prototype  Display the prototype text without modification")
+  echo("  --no-parens  Don't use parentheses")
+  echo("  --no-this    Don't use 'This is a' in front of the text")
+
+proc quit_cmd_option_unnecessary_value(option: string) =
+  quit(fmt"Command line option '{option}' doesn't take a value", 1)
+
+proc direct_output(output: string) =
+  quit(output, 0)
+
+const long_no_val = @[
+  "help",
+  "version",
+  "prototype",
+  "no-parens",
+  "no-this"
+]
+var p = po.initOptParser(shortNoVal = {}, longNoVal = long_no_val)
+while true:
+  p.next()
+  when DEBUG:
+    stdout.write(fmt"Option: ({p.kind})")
+    if p.kind != po.cmdEnd:
+      stdout.write(fmt" '{p.key}' = '{p.val}'")
+    echo()
+  case p.kind
+    of po.cmdEnd:
+      break
+    of po.cmdShortOption, po.cmdLongOption:
+      if p.key in long_no_val and p.val != "":
+        quit_cmd_option_unnecessary_value(p.key)
+      case p.key:
+        of "help":
+          show_help_only = true
+        of "version":
+          show_version_only = true
+        of "prototype":
+          show_prototype = true
+        of "no-parens":
+          use_parens = false
+        of "no-this":
+          use_this = false
+        else:
+          quit(fmt"Unrecognized command line option '{p.key}'", 1)
+    of po.cmdArgument:
+      quit(fmt"This program doesn't take any non-option arguments: '{p.key}'", 1)
+
+if show_help_only:
+  show_help()
+  quit(0)
+
+if show_version_only:
+  direct_output(version(full=true))
+
+if show_prototype:
+  direct_output(PROTOTYPE)
+
+const
+  this = "this is a"
+  verb = "keep"
+  particle = "from"
+  article = "the"
+  noun = "timeline"
+  last_part = "clearing"
+
+var
+  output = fmt"placeholder to {verb} {article} {noun} {particle} {last_part}"
 
 if use_this:
-    output = fmt"{this} {output}"
+  output = fmt"{this} {output}"
 
 if use_parens:
-    output = fmt"({output})"
+  output = fmt"({output})"
 
 echo(output)
